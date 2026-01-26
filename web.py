9d09d8c3a8432f4bf8759b8cb59b28f14a725870
MARINE <hujaifs31@gmail.com>
2026-01-26 18:32:19 +0530

    Update web.py

--- web.py
+++ web.py
@@ -10,10 +10,4 @@ class Handler(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(b"ChikooMusic is running!")
 
-def run():
-    server = HTTPServer(("0.0.0.0", PORT), Handler)
-    print(f"Web server running on 0.0.0.0:{PORT}")
-    server.serve_forever()
-
-if __name__ == "__main__":
-    run()
+HTTPServer(("0.0.0.0", PORT), Handler).serve_forever()
