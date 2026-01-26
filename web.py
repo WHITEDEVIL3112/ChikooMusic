1182f750c22fe0dbbf76cfbf207b4687cdb25cfe
MARINE <hujaifs31@gmail.com>
2026-01-26 17:01:11 +0530

    Create web.py

---
+++ web.py
@@ -0,0 +1,19 @@
+import os
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+PORT = int(os.environ.get("PORT", "10000"))
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        self.wfile.write(b"ChikooMusic is running!")
+
+def run():
+    server = HTTPServer(("0.0.0.0", PORT), Handler)
+    print(f"Web server running on port {PORT}")
+    server.serve_forever()
+
+if __name__ == "__main__":
+    run()
