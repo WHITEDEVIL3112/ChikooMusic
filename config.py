2f7d23ce356a4a44b709b715ac776850f05951d3
MARINE <hujaifs31@gmail.com>
2026-02-02 16:46:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -46,18 +46,21 @@ STRING5 = os.getenv("STRING_SESSION5", None)
 
 AUTO_LEAVING_ASSISTANT = bool(os.getenv("AUTO_LEAVING_ASSISTANT", False))
 
-START_IMG_URL = os.getenv("START_IMG_URL", "https://files.catbox.moe/7q8bfg.jpg")
-PING_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-PLAYLIST_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-STATS_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-TELEGRAM_AUDIO_URL = "https://files.catbox.moe/eehxb4.jpg"
-TELEGRAM_VIDEO_URL = "https://files.catbox.moe/eehxb4.jpg"
-STREAM_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-SOUNCLOUD_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-YOUTUBE_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
+# ✅ FIXED: Telegram-safe Telegraph URLs
+SAFE_IMG = "https://telegra.ph/file/96f36504f149e5680741a.jpg"
+
+START_IMG_URL = os.getenv("START_IMG_URL", SAFE_IMG)
+PING_IMG_URL = SAFE_IMG
+PLAYLIST_IMG_URL = SAFE_IMG
+STATS_IMG_URL = SAFE_IMG
+TELEGRAM_AUDIO_URL = SAFE_IMG
+TELEGRAM_VIDEO_URL = SAFE_IMG
+STREAM_IMG_URL = SAFE_IMG
+SOUNCLOUD_IMG_URL = SAFE_IMG
+YOUTUBE_IMG_URL = SAFE_IMG
+SPOTIFY_ARTIST_IMG_URL = SAFE_IMG
+SPOTIFY_ALBUM_IMG_URL = SAFE_IMG
+SPOTIFY_PLAYLIST_IMG_URL = SAFE_IMG
 
 BANNED_USERS = filters.user()
 adminlist = {}
